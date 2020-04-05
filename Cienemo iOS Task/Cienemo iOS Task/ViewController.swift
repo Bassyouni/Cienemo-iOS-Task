9a41173ffd3b472eab28a0457e554fae49e3cfb6
Omar Bassyouni <omarbassyouni991@gmail.com>
2020-04-05 10:58:19 +0200

    add projec to project folder

---
+++ Cienemo iOS Task/Cienemo iOS Task/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cienemo iOS Task
+//
+//  Created by Omar Bassyouni on 4/5/20.
+//  Copyright © 2020 Omar Bassyouni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
